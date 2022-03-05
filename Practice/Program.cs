a2718b72c99d46d21481c06d0e20fa25ac714d08
yburkul <yburkul123@gmail.com>
2022-03-05 17:09:02 +0530

    Added:Flip coin problrm

---
+++ Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int Coin_Head = 1;
+Random random = new Random();
+int coinCheck = random.Next(0, 2);
+if (coinCheck == Coin_Head)
+{
+    Console.WriteLine("Coin is Head");
+
+}
+else
+{
+    Console.WriteLine("Coin is Tails");
+
+}
